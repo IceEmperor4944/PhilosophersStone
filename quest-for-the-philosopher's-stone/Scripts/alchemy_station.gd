d4e424d40780ca707223891c6825961c6e6fb4f7
Lace-Gang <rhall@student.neumont.edu>
2024-11-19 15:58:14 -0700

    changed alchemy table to signal based

--- quest-for-the-philosopher's-stone/Scripts/alchemy_station.gd
+++ quest-for-the-philosopher's-stone/Scripts/alchemy_station.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+signal element_created(recipe)
 
-var activeInput = [[0, 0, 0, 0], [0, 0, 0, 0]]
+var activeInput = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
 var numSlots = 4 #number of elements in a recipe
 
@@ -13,43 +14,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+# Ready to attach to a signal
+# Recieves a signal with arguments(recipe, position)
+# Where recipe is the recipe of the element/potion that is being added
+# And position is which part of the array it is being added to
+	# Each position in the array will correspond to exactly one part of the alchemy table (where you can place elements into it)
+func on_add_ingredient(recipe, position):
+	activeInput[position] = recipe
 
-# please input RECIPE of ingredient, NOT name
-func AddInput(recipe):
-	var success = false
-	for element in activeInput:
-		if element == [0, 0, 0, 0]:
-			element = recipe
-			success = true
-			break
-	return success
+# ready to attach to necessary signal
+# for use when an ingredient is removed from the table BUT IS NOT REPLACED
+func on_ingredient_removed(position):
+	activeInput[position] = [0, 0, 0, 0]
 	
-# please input RECIPE of ingredient, NOT name
-
-func RemoveInput(recipe):
-	var success = false
-	for element in activeInput:
-		if element == recipe:
-			element = [0, 0, 0, 0]
-			success = true
-			break
-	return success
-	
-# please input RECIPE of ingredient, NOT name
-#If remove recipe is known, and add recipe is not, please enter [0, 0, 0, 0] for add recipe
-func SwapInput(addRecipe = [0, 0, 0, 0], removeRecipe = [0, 0, 0, 0]):
-	var success = false
-	for element in activeInput:
-		if element == removeRecipe:
-			element = addRecipe
-			success = true
-			break
-	return success
-	
-	
-func addActiveIngredients():
+# ready to attach to a signal
+# for use when alchemy table is activated to combine what it currently on it
+func on_Combine_Ingredients():
 	var numTypes = 0
-	
 	
 	#next variables are for each element that can be in the recipe
 	var newRecipe = [0, 0, 0, 0]
@@ -63,5 +45,42 @@ func addActiveIngredients():
 			newRecipe[i] += activeInput[j][i]
 		newRecipe[i] /= numTypes
 	
-	return newRecipe
+	element_created.emit(newRecipe)
+	#return newRecipe
+
+# please input RECIPE of ingredient, NOT name
+# This function is likely obsolete
+#func AddInput(recipe):
+#	var success = false
+#	for element in activeInput:
+#		if element == [0, 0, 0, 0]:
+#			element = recipe
+#			success = true
+#			break
+#	return success
+	
+# please input RECIPE of ingredient, NOT name
+# This function is likely obsolete
+#func RemoveInput(recipe):
+#	var success = false
+#	for element in activeInput:
+#		if element == recipe:
+#			element = [0, 0, 0, 0]
+#			success = true
+#			break
+#	return success
+	
+	# please input RECIPE of ingredient, NOT name
+	# If remove recipe is known, and add recipe is not, please enter [0, 0, 0, 0] for add recipe
+	# This function is likely obsolete
+#func SwapInput(addRecipe = [0, 0, 0, 0], removeRecipe = [0, 0, 0, 0]):
+#	var success = false
+#	for element in activeInput:
+#		if element == removeRecipe:
+#			element = addRecipe
+#			success = true
+#			break
+#	return success
+	
+
 		
