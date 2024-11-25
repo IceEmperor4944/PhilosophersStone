43144ede9aca2318b5f3796ba504cc9ebfc7203b
neote250 <nornano@student.neumont.edu>
2024-11-25 16:07:50 -0700

    clean up stuff
    cleaning up some of my code

--- quest-for-the-philosopher's-stone/Scripts/example_main.gd
+++ quest-for-the-philosopher's-stone/Scripts/example_main.gd
@@ -6,37 +6,20 @@ var entered3 = false
 var entered4 = false
 
 #region Ready and Update
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#$HUD.emitHandButtonPressed.connect(spawn)
-	#var ingredientMngr = IngredientManager.new()
-	#var testList = ingredientMngr.list
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	###if entered && Input.mouseclick ==false
-		
+	
 	pass
 #endregion
-func spawn(pos: Vector2i):
-	var instance = IngredientSpawnedScene.instantiate()
-	add_child(instance)
-	#ingredientsCollection.add_child(instance)
-	
-
-
-
-
-func _on_slider_area_1_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	entered1 = true
-	
-
-func _on_slider_area_1_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	entered1 = false
-
-
 func _on_hud_emit_hand_button_pressed() -> void:
 	var instance = IngredientSpawnedScene.instantiate()
 	add_child(instance)
+
+func _on_slider_area_1_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	entered1 = true
+
+func _on_slider_area_1_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	entered1 = false
