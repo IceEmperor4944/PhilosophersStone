1fe22aefad42491c038bf83191ada2faad2c3223
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2024-12-02 14:55:37 -0700

    death

--- quest-for-the-philosopher's-stone/Scripts/waterspawner.gd
+++ quest-for-the-philosopher's-stone/Scripts/waterspawner.gd
@@ -1,34 +1,24 @@
 extends Ingredient
 class_name Spawner 
 
-#have objects in display in an array
-
-@export var speed = 400;
 var screen_size;
 var has_mouse:bool = false;
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size;
-	
-	
 func _input(event):
 	if event is InputEventMouseButton:
 		if Input.is_action_pressed("pick_up") && (get_global_mouse_position().x <= $Sprite2D.global_position.x + $Sprite2D.get_parent().position.x/10) && (get_global_mouse_position().x >= $Sprite2D.global_position.x - $Sprite2D.get_parent().position.x /10):
 			if (get_global_mouse_position().y <= $Sprite2D.global_position.y + $Sprite2D.get_parent().position.y/5) && (get_global_mouse_position().y >= $Sprite2D.global_position.y - $Sprite2D.get_parent().position.y /5):
 				ingredient.position = position;
-				
+
 				has_mouse = true;
 		elif (Input.is_action_just_released("pick_up")):
 			has_mouse = false;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size;
+
 				
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	var velocity = Vector2.ZERO
-	
 	if (has_mouse):
 		position = get_global_mouse_position();
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
