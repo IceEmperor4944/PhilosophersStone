2376cdd7adb77b256bdc76916edbfb18e632a22d
neote250 <nornano@student.neumont.edu>
2024-12-02 15:03:27 -0700

    clean up

--- quest-for-the-philosopher's-stone/Scripts/test_main.gd
+++ quest-for-the-philosopher's-stone/Scripts/test_main.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @export var scene : PackedScene
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,3 +14,7 @@ func _process(delta: float) -> void:
 func _on_hud_emit_hand_button_pressed(pos: Variant) -> void:
 	var this_scene = scene.instantiate();
 	add_child(this_scene);
+
+
+func _on_water_spawner_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
