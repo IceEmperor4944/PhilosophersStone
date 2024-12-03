888677db04e69b3b90e2c3712c703838c46c4f6c
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2024-12-03 15:17:32 -0700

    raaaaaaaaa

--- quest-for-the-philosopher's-stone/Scripts/waterspawner.gd
+++ quest-for-the-philosopher's-stone/Scripts/waterspawner.gd
@@ -4,7 +4,7 @@ class_name Spawner
 #have objects in display in an array
 
 #@export var sprite = Sprite2D.new();
-@export var speed = 400;
+
 var screen_size;
 var has_mouse:bool = false;
 
@@ -30,7 +30,6 @@ func _input(event):
 		if Input.is_action_pressed("pick_up") && (get_global_mouse_position().x <= $Sprite2D.global_position.x + $Sprite2D.get_parent().position.x/10) && (get_global_mouse_position().x >= $Sprite2D.global_position.x - $Sprite2D.get_parent().position.x /10):
 			if (get_global_mouse_position().y <= $Sprite2D.global_position.y + $Sprite2D.get_parent().position.y/5) && (get_global_mouse_position().y >= $Sprite2D.global_position.y - $Sprite2D.get_parent().position.y /5):
 				ingredient.position = position;
-				
 				has_mouse = true;
 		elif (Input.is_action_just_released("pick_up")):
 			has_mouse = false;
@@ -39,13 +38,8 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var velocity = Vector2.ZERO
-	
 	if (has_mouse):
 		position = get_global_mouse_position();
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	
 
