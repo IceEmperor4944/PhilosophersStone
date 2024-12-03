2d06ff2c6c3e17c92b4833f41f20161426c1c1f8
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2024-12-03 15:18:27 -0700

    Squashed commit of the following:
    commit ae81ba7d375bd5d6b79dc9ae61790ca409b584c3
    Merge: 7fc3699 d6f6a05
    Author: neote250 <nornano@student.neumont.edu>
    Date:   Tue Dec 3 15:13:24 2024 -0700
    
        Merge branch 'master' into NephiOrnano
    
    commit 7fc3699d07e445ce4259ea834cdcbe3102e57015
    Author: neote250 <nornano@student.neumont.edu>
    Date:   Tue Dec 3 15:11:22 2024 -0700
    
        combine button and inventory start
    
    commit d6f6a05f82db865e2a55c29ebcb7281598f2200a
    Author: TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
    Date:   Mon Dec 2 15:53:38 2024 -0700
    
        please work please
    
    commit 2486ade42a10ad38aa1b79e0dc039e6ed710447a
    Author: TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
    Date:   Mon Dec 2 15:29:30 2024 -0700
    
        Squashed commit of the following:
    
        commit 15a23daedcbf9466cd9cfb9aa68c90d4a94abbd9
        Author: TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
        Date:   Mon Dec 2 15:28:18 2024 -0700
    
            Update to Fix Mouse (not functional)
    
    commit e534834c8ae2af0d4c46f193b76b8dba2c5f0283
    Author: TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
    Date:   Mon Dec 2 15:29:02 2024 -0700
    
        Squashed commit of the following:
    
        commit 82f688d8fad121db168eb79f0599fc6534d2e75e
        Author: neote250 <nornano@student.neumont.edu>
        Date:   Mon Dec 2 15:26:27 2024 -0700
    
            fixed path
    
        commit be1c7cb8c56c991abb42f9a446953f8301b406f7
        Merge: 2376cdd bd6dd79
        Author: neote250 <nornano@student.neumont.edu>
        Date:   Mon Dec 2 15:04:54 2024 -0700
    
            Merge branch 'master' into NephiOrnano
    
        commit 2376cdd7adb77b256bdc76916edbfb18e632a22d
        Author: neote250 <nornano@student.neumont.edu>
        Date:   Mon Dec 2 15:03:27 2024 -0700
    
            clean up

--- quest-for-the-philosopher's-stone/Scripts/ingredient_main.gd
+++ quest-for-the-philosopher's-stone/Scripts/ingredient_main.gd
@@ -2,71 +2,19 @@ extends Node2D
 
 @export var scene : PackedScene
 
-signal onIngredientAdded(type, position)
-
-#This will be a number to signify which area it is in (if any) and will only be changed via signals
-var inArea;
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	inArea = -1;
-	pass # Replace with function body.
+	$WaterSpawner._ready() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$WaterSpawner._process(delta)
 
 
+func _input(event):
+	$WaterSpawner._input(event)
+	
 func _on_hud_emit_hand_button_pressed(pos: Variant) -> void:
 	var this_scene = scene.instantiate();
 	add_child(this_scene);
-
-
-func _on_water_spawner_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	
-	pass # Replace with function body.
-
-
-func _on_water_spawner_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	pass # Replace with function body.
-
-#The below signals are to detect if/when the ingredient is in a location where it can be placed into the alchemy table
-
-func _on_slider_area_1_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = 0
-
-
-func _on_slider_area_1_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = -1
-
-
-func _on_slider_area_2_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = 1
-
-
-func _on_slider_area_2_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = -1
-
-
-func _on_slider_area_3_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = 2
-
-
-func _on_slider_area_3_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = -1
-
-
-func _on_slider_area_4_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = 3
-
-
-func _on_slider_area_4_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	inArea = -1
-
-#this method gets information to the alchemy table when an ingredient is released within one of the slider areas
-
-func _on_water_spawner_on_click_released(typeName: Variant) -> void:
-	if(inArea >= 0 && inArea <= 3):
-		onIngredientAdded.emit(typeName, inArea)
-		queue_free()
